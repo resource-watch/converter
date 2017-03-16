fb6796e3f0de9502d882ca290688a5f7b8b8ff1a
rrequero <rrequero@gmail.com>
2017-03-16 17:16:53 +0100

    add check count(*)

---
+++ app/src/errors/queryNotValid.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class QueryNotValid extends Error{
+
+    constructor(code, message){
+        super(message);
+        this.status = code;
+        this.name = 'GeoStoreNotFound';
+        this.message = message;
+    }
+}
+module.exports = QueryNotValid;
--- app/src/services/converterService.js
+++ app/src/services/converterService.js
@@ -6,6 +6,7 @@ var geojsonToArcGIS = require('arcgis-to-geojson-utils').geojsonToArcGIS;
 var arcgisToGeoJSON = require('arcgis-to-geojson-utils').arcgisToGeoJSON;
 const Sql2json = require('sql2json').sql2json;
 const Json2sql = require('sql2json').json2sql;
+const QueryNotValid = require('errors/queryNotValid');
 
 const aggrFunctions = ['count', 'sum', 'min', 'max', 'avg', 'stddev', 'var'];
 const aggrFunctionsRegex = /(count *\(|sum\(|min\(|max\(|avg\(|stddev\(|var\(){1}[A-Za-z0-9_]*/g;
@@ -281,6 +282,9 @@ class ConverterService {
           if (node.value.toLowerCase() === 'count' && parsed.select.length === 1) {
             fs.returnCountOnly = true;
           } else {
+            if (node.value.toLowerCase() === 'count' && node.arguments[0].value === '*'){
+              throw new QueryNotValid(400, 'Invalid query. ArcGis does not support count(*) with more columns');
+            }
             let obj = {
               statisticType: node.value,
               onStatisticField: node.arguments[0].value
